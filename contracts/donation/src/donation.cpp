1df89c4999d3914a0d34e005dbeca063e61124ed
Michael Yeates <michaeljyeates@gmail.com>
2019-09-02 01:57:01 +0100

    Do not accept ram returns

--- contracts/donation/src/donation.cpp
+++ contracts/donation/src/donation.cpp
@@ -8,21 +8,23 @@ namespace eosdao {
             const asset&   quantity,
             const string&  memo )
     {
-        if ( to == get_self() ){
-            check(quantity.symbol == symbol(symbol_code("EOS"), 4), "Only EOS payments are accepted");
-            check(quantity.amount >= 10000, "ERR::QUANTITY_TOO_LOW::Minimum donation is 1.0000 EOS");
-
-            // Receiving donation, send inline action to issue tokens back to the sender
-            const asset dao_quantity = asset(quantity.amount, symbol{symbol_code("DAO"), 4});
-
-            string memo = "Thank you for your donation to EOS DAO";
-
-            eosio::action(
-                    eosio::permission_level{ "oadtestingtk"_n, "issue"_n },
-                    "oadtestingtk"_n, "issue"_n,
-                    make_tuple(from, dao_quantity, memo)
-            ).send();
+        if (to != _self || from == "eosio"_n || from == "eosio.ram"_n || from == "eosio.stake"_n){
+            return;
         }
+
+        check(quantity.symbol == symbol(symbol_code("EOS"), 4), "Only EOS payments are accepted");
+        check(quantity.amount >= 10000, "ERR::QUANTITY_TOO_LOW::Minimum donation is 1.0000 EOS");
+
+        // Receiving donation, send inline action to issue tokens back to the sender
+        const asset dao_quantity = asset(quantity.amount, symbol{symbol_code("DAO"), 4});
+
+        string memo = "Thank you for your donation to EOS DAO";
+
+        eosio::action(
+                eosio::permission_level{ "oadtestingtk"_n, "issue"_n },
+                "oadtestingtk"_n, "issue"_n,
+                make_tuple(from, dao_quantity, memo)
+        ).send();
     }
 
     void donation::dummy(){}
